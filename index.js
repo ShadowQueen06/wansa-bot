852a8fcfb9d6fcf2adad8def2deb023ad3627403
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-01 00:05:52 +0200

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const {
+    Client,
+    GatewayIntentBits,
+    EmbedBuilder
+} = require("discord.js");
 
 const client = new Client({
     intents: [
@@ -8,8 +12,40 @@ const client = new Client({
     ]
 });
 
-client.once("ready", () => {
+client.once("clientReady", () => {
     console.log(`✅ ${client.user.tag} شغال`);
+});
+
+client.on("messageCreate", async (message) => {
+
+    if (message.author.bot) return;
+
+    if (message.content === "-العاب") {
+
+        const embed = new EmbedBuilder()
+            .setColor("#7B2CBF")
+            .setTitle("🎮 ونسة")
+            .setDescription(`
+أهلًا بك في ونسة.
+
+اختر اللعبة التي تريدها.
+
+━━━━━━━━━━━━━━
+
+🎭 كت
+❌⭕ إكس أو
+❓ أسئلة عامة
+🎲 ألعاب فردية
+
+قريبًا سيتم إضافة المزيد من الألعاب.
+            `);
+
+        message.reply({
+            embeds: [embed]
+        });
+
+    }
+
 });
 
 client.login(process.env.TOKEN);
