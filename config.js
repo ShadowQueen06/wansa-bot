b8b290c24e2ee3fa48630d8c83cbcac34b54958d
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-01 02:31:13 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    prefix: "-",
+    color: "#7B2CBF",
+    rewards: {
+        quizCorrectCoins: 10,
+        quizCorrectXp: 5,
+        xoWinCoins: 25,
+        xoWinXp: 15,
+        soloCoins: 8,
+        soloXp: 4
+    }
+};
