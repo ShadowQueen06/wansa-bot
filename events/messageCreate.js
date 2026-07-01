22e5b9171ef0632ccff473f4a251b5931ca126ea
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-01 02:47:04 +0200

    messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,7 @@
 const { prefix } = require("../config.js");
 const { mainMenuEmbed, mainMenuButtons } = require("../utils/menu.js");
 const xo = require("../games/xo.js");
+const kat = require("../games/kat.js");
 
 module.exports = async (message) => {
     if (message.author.bot) return;
@@ -10,6 +11,13 @@ module.exports = async (message) => {
         return message.reply({
             embeds: [mainMenuEmbed()],
             components: mainMenuButtons()
+        });
+    }
+
+    if (message.content === `${prefix}كت`) {
+        return message.reply({
+            embeds: [kat.questionEmbed(message.author)],
+            components: kat.menuButtons()
         });
     }
 
